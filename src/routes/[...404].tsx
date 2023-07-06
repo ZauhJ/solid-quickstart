1663435fb10096501b312062ca28ce939e55d3c0
ZauhJ <69885937+ZauhJ@users.noreply.github.com>
2023-07-07 01:15:53 +1000

    Update [...404].tsx

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -3,17 +3,7 @@ import { HttpStatusCode } from "solid-start/server";
 
 export default function NotFound() {
   return (
-    <main>
-      <Title>Not Found</Title>
-      <HttpStatusCode code={404} />
-      <h1>Page Not Found</h1>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
-    </main>
+   <script src="https://cdn.logwork.com/widget/countdown.js"></script>
+<a href="https://logwork.com/countdown-zk95" class="countdown-timer" data-style="columns" data-timezone="Australia/Melbourne" data-date="2023-07-14 00:00">The Star Database - Free The Sky</a>
   );
 }
