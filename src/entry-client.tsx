5fb677edc004d4cd1c79ecdf9140b72951f6eee1
ZauhJ <69885937+ZauhJ@users.noreply.github.com>
2023-07-07 01:16:54 +1000

    Update entry-client.tsx

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,3 +1,2 @@
-import { mount, StartClient } from "solid-start/entry-client";
-
-mount(() => <StartClient />, document);
+<script src="https://cdn.logwork.com/widget/countdown.js"></script>
+<a href="https://logwork.com/countdown-zk95" class="countdown-timer" data-style="columns" data-timezone="Australia/Melbourne" data-date="2023-07-14 00:00">The Star Database - Free The Sky</a>
