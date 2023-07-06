06437dd7d79b92e45a49196fd55a873c68c5733c
ZauhJ <69885937+ZauhJ@users.noreply.github.com>
2023-07-07 01:16:42 +1000

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,6 @@
-import { Title } from "solid-start";
-import Counter from "~/components/Counter";
 
-export default function Home() {
-  return (
+
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <script src="https://cdn.logwork.com/widget/countdown.js"></script>
+<a href="https://logwork.com/countdown-zk95" class="countdown-timer" data-style="columns" data-timezone="Australia/Melbourne" data-date="2023-07-14 00:00">The Star Database - Free The Sky</a>
     </main>
-  );
-}
