558abf61ffda03f96238925fc641023954674513
ZauhJ <69885937+ZauhJ@users.noreply.github.com>
2023-07-07 01:14:11 +1000

    Update basic.cy.ts

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -3,13 +3,6 @@ describe('empty spec', () => {
     cy.visit('/')
   })
 
-  it('display header text', () => {
-    cy.get('h1')
-    .contains('Hello World!', { matchCase: false })
-  })
-
-  it('displays Click button', () => {
-    cy.get('button')
-    .contains('Clicks');
-  })
+ <script src="https://cdn.logwork.com/widget/countdown.js"></script>
+<a href="https://logwork.com/countdown-zk95" class="countdown-timer" data-style="columns" data-timezone="Australia/Melbourne" data-date="2023-07-14 00:00">The Star Database - Free The Sky</a>
 })
